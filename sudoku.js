43fa2a733d5376f1417b1b9db86346eb2feb0fee
GUNUPURU GUNASHEKAR SAI <113043690+gunasekharsai@users.noreply.github.com>
2023-06-23 17:01:03 +0530

    changes

---
+++ sudoku.js
@@ -0,0 +1,93 @@
+var numSelected =null;
+var titleSelected = null;
+var errors =0;
+var board=[
+    "------2--",
+    "-8---7-9-",
+    "6-2---5--",
+    "-7--6----",
+    "---9-1---",
+    "----2--4-",
+    "--5---6-3",
+    "-9-4---7-",
+    "--6------",
+]
+
+var solutions = [
+    "957613284",
+    "483257196",
+    "612849537",
+    "178364952",
+    "524971368",
+    "369528741",
+    "845792613",
+    "291436875",
+    "736185429",
+]
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    // Digits 1-9
+    for (let i = 1; i <= 9; i++) {
+        //<div id="1" class="number">1</div>
+        let number = document.createElement("div");
+        number.id = i
+        number.innerText = i;
+        number.addEventListener("click", selectNumber);
+        number.classList.add("number");
+        document.getElementById("digits").appendChild(number);
+    }
+
+    // Board 9x9
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            if (board[r][c] != "-") {
+                tile.innerText = board[r][c];
+                tile.classList.add("tile-start");
+            }
+            if (r == 2 || r == 5) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c == 2 || c == 5) {
+                tile.classList.add("vertical-line");
+            }
+            tile.addEventListener("click", selectTile);
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
+        }
+    }
+}
+
+function selectNumber(){
+    if (numSelected != null) {
+        numSelected.classList.remove("number-selected");
+    }
+    numSelected = this;
+    numSelected.classList.add("number-selected");
+}
+
+function selectTile() {
+    if (numSelected) {
+        if (this.innerText != "") {
+            return;
+        }
+
+        // "0-0" "0-1" .. "3-1"
+        let coords = this.id.split("-"); //["0", "0"]
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
+
+        if (solution[r][c] == numSelected.id) {
+            this.innerText = numSelected.id;
+        }
+        else {
+            errors += 1;
+            document.getElementById("errors").innerText = errors;
+        }
+    }
+}
